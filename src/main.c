bdd8c58a4c2485e0429eef605c91e4816eba7974
betaEncoder <betaencoder@gmail.com>
2020-05-11 17:11:17 +0900

    skelton

---
+++ src/main.c
@@ -0,0 +1,23 @@
+/********************************** (C) COPYRIGHT *******************************
+* File Name		  : main.c
+* Author			 : betaEncoder
+* Version			: V1.0
+* Date			   : 2020/05/11
+* Description		: uart sample
+*******************************************************************************/
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+#include <ch554.h>
+#include <ch554_usb.h>
+#include <debug.h>
+
+#include "uart.h"
+
+int main(){
+
+    while(1);
+
+    return 0;
+}
